a28807cd98899ea5d913111d31b5d2cd075bc764
Evgeny Novoselov <zkodru@gmail.com>
2022-11-06 22:29:35 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# job4j_dreamjob
+[![Java CI with Maven](https://github.com/zkod/job4j_dreamjob/actions/workflows/maven.yml/badge.svg)](https://github.com/zkod/job4j_dreamjob/actions/workflows/maven.yml)
---
+++ src/test/java/ru/job4j/dreamjob/AppTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.dreamjob;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
