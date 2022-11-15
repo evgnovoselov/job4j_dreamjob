a84ddb6b632ca51490d0c53bd40d378a22501e6c
Evgeny Novoselov <zkodru@gmail.com>
2022-11-15 21:37:37 +0300

    Исправил работу города у вакансий

--- src/main/java/ru/job4j/dreamjob/controller/PostController.java
+++ src/main/java/ru/job4j/dreamjob/controller/PostController.java
@@ -3,16 +3,10 @@ package ru.job4j.dreamjob.controller;
 import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import ru.job4j.dreamjob.model.City;
+import org.springframework.web.bind.annotation.*;
 import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.service.CityService;
 import ru.job4j.dreamjob.service.PostService;
-
-import java.time.LocalDateTime;
 
 @Controller
 @ThreadSafe
@@ -33,14 +27,13 @@ public class PostController {
 
     @GetMapping("/formAddPost")
     public String formAddPost(Model model) {
-        model.addAttribute("post", new Post(0, "Заполните значение", "Заполните описание",
-                new City(5, "Ереван"), LocalDateTime.now()));
         model.addAttribute("cities", cityService.getAllCities());
         return "addPost";
     }
 
     @PostMapping("/createPost")
     public String createPost(@ModelAttribute Post post) {
+        post.setCity(cityService.findById(post.getCity().getId()));
         postService.add(post);
         return "redirect:/posts";
     }
@@ -54,6 +47,7 @@ public class PostController {
 
     @PostMapping("/updatePost")
     public String updatePost(@ModelAttribute Post post) {
+        post.setCity(cityService.findById(post.getCity().getId()));
         postService.update(post);
         return "redirect:/posts";
     }
