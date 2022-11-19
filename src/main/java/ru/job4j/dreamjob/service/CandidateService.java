565143bb0a124b77e9bf65ac2535cd54824221d4
Evgeny Novoselov <zkodru@gmail.com>
2022-11-19 10:02:25 +0300

    Реализовал хранение кандидатов в БД

--- src/main/java/ru/job4j/dreamjob/service/CandidateService.java
+++ src/main/java/ru/job4j/dreamjob/service/CandidateService.java
@@ -3,7 +3,7 @@ package ru.job4j.dreamjob.service;
 import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Service;
 import ru.job4j.dreamjob.model.Candidate;
-import ru.job4j.dreamjob.store.CandidateStore;
+import ru.job4j.dreamjob.store.CandidateDBStore;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
@@ -11,26 +11,36 @@ import java.util.Collection;
 @Service
 @ThreadSafe
 public class CandidateService {
-    private final CandidateStore candidateStore;
+    private final CandidateDBStore store;
+    private final CityService cityService;
 
-    public CandidateService(CandidateStore candidateStore) {
-        this.candidateStore = candidateStore;
+    public CandidateService(CandidateDBStore store, CityService cityService) {
+        this.store = store;
+        this.cityService = cityService;
     }
 
     public Collection<Candidate> findAll() {
-        return candidateStore.findAll();
+        Collection<Candidate> candidates = store.findAll();
+        candidates.forEach(candidate -> candidate.setCity(cityService.findById(candidate.getCity().getId())));
+        return candidates;
     }
 
     public Candidate findById(int id) {
-        return candidateStore.findById(id);
+        Candidate candidate = store.findById(id);
+        candidate.setCity(cityService.findById(candidate.getCity().getId()));
+        return candidate;
     }
 
     public void add(Candidate candidate) {
         candidate.setCreated(LocalDateTime.now());
-        candidateStore.add(candidate);
+        store.add(candidate);
     }
 
     public void update(Candidate candidate) {
-        candidateStore.update(candidate);
+        Candidate oldCandidate = findById(candidate.getId());
+        if (candidate.getPhoto().length == 0 && oldCandidate.getPhoto().length > 0) {
+            candidate.setPhoto(oldCandidate.getPhoto());
+        }
+        store.update(candidate);
     }
 }
