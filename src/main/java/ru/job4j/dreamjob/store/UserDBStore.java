8f435a23fd2ca37ed34abe6975fd7ecc64146943
Evgeny Novoselov <zkodru@gmail.com>
2022-11-20 18:54:49 +0300

    Добавил возможность зарегистрировать пользователя

---
+++ src/main/java/ru/job4j/dreamjob/store/UserDBStore.java
@@ -0,0 +1,46 @@
+package ru.job4j.dreamjob.store;
+
+import net.jcip.annotations.ThreadSafe;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import ru.job4j.dreamjob.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@Repository
+@ThreadSafe
+public class UserDBStore {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserDBStore.class.getSimpleName());
+    private static final String SQL_ADD_USER = "INSERT INTO \"user\"(email, password) VALUES (?,?)";
+    private final BasicDataSource pool;
+
+    public UserDBStore(BasicDataSource pool) {
+        this.pool = pool;
+    }
+
+    public User add(User user) {
+        User result = null;
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps = cn.prepareStatement(
+                     SQL_ADD_USER,
+                     PreparedStatement.RETURN_GENERATED_KEYS
+             )) {
+            ps.setString(1, user.getEmail());
+            ps.setString(2, user.getPassword());
+            ps.execute();
+            try (ResultSet id = ps.getGeneratedKeys()) {
+                if (id.next()) {
+                    user.setId(id.getInt(1));
+                }
+            }
+            result = user;
+        } catch (Exception e) {
+            LOGGER.error("Error UserDBStore.add User email = {}", user.getEmail());
+        }
+        return result;
+    }
+}
