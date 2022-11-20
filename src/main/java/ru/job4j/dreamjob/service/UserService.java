2a8b825d9ca81ac657b475e483dcf792845303fb
Evgeny Novoselov <zkodru@gmail.com>
2022-11-20 22:37:46 +0300

    Поправил шаблон входа и добавил методы в контроллер и сервис

--- src/main/java/ru/job4j/dreamjob/service/UserService.java
+++ src/main/java/ru/job4j/dreamjob/service/UserService.java
@@ -19,4 +19,8 @@ public class UserService {
     public Optional<User> add(User user) {
         return Optional.ofNullable(store.add(user));
     }
+
+    public Optional<User> findUserByEmailAndPassword(String email, String password) {
+        return Optional.empty();
+    }
 }
