95b1a48707e3442ad3819fdefe54cfbb13cf9fc4
Evgeny Novoselov <zkodru@gmail.com>
2022-11-22 22:38:11 +0300

    Написал тест контроллера метода posts

---
+++ src/test/java/ru/job4j/dreamjob/controller/PostControllerTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.dreamjob.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.ui.Model;
+import ru.job4j.dreamjob.model.City;
+import ru.job4j.dreamjob.model.Post;
+import ru.job4j.dreamjob.service.CityService;
+import ru.job4j.dreamjob.service.PostService;
+
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+class PostControllerTest {
+    @Test
+    void whenPosts() {
+        List<Post> posts = List.of(
+                new Post(1, "New post", "Description", new City(1, "Moscow"),
+                        true, LocalDateTime.now()),
+                new Post(2, "New post", "Description", new City(1, "Moscow"),
+                        true, LocalDateTime.now())
+        );
+        Model model = mock(Model.class);
+        PostService postService = mock(PostService.class);
+        when(postService.findAll()).thenReturn(posts);
+        CityService cityService = mock(CityService.class);
+        PostController postController = new PostController(postService, cityService);
+        HttpSession session = mock(HttpSession.class);
+        String page = postController.posts(model, session);
+        verify(model).addAttribute("posts", posts);
+        assertThat(page).isEqualTo("posts");
+    }
+}
