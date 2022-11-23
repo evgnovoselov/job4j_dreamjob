75c265f779f3baed046a119609240f0a0d99fef4
Evgeny Novoselov <zkodru@gmail.com>
2022-11-24 00:10:26 +0300

    Дописал тест без пользователя

--- src/test/java/ru/job4j/dreamjob/controller/PostControllerTest.java
+++ src/test/java/ru/job4j/dreamjob/controller/PostControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.ui.Model;
 import ru.job4j.dreamjob.model.City;
 import ru.job4j.dreamjob.model.Post;
+import ru.job4j.dreamjob.model.User;
 import ru.job4j.dreamjob.service.CityService;
 import ru.job4j.dreamjob.service.PostService;
 
@@ -23,14 +24,38 @@ public class PostControllerTest {
                 new Post(2, "New post", "Description", new City(1, "Moscow"),
                         true, LocalDateTime.now())
         );
+        User user = new User(1, "User", "example@example.com", "password");
         Model model = mock(Model.class);
         PostService postService = mock(PostService.class);
         when(postService.findAll()).thenReturn(posts);
+        HttpSession session = mock(HttpSession.class);
+        when(session.getAttribute("user")).thenReturn(user);
         CityService cityService = mock(CityService.class);
         PostController postController = new PostController(postService, cityService);
-        HttpSession session = mock(HttpSession.class);
         String page = postController.posts(model, session);
         verify(model).addAttribute("posts", posts);
+        verify(model).addAttribute("user", user);
+        assertThat(page).isEqualTo("posts");
+    }
+
+    @Test
+    public void whenPostsWithoutUser() {
+        List<Post> posts = List.of(
+                new Post(1, "New post", "Description", new City(1, "Moscow"),
+                        true, LocalDateTime.now()),
+                new Post(2, "New post", "Description", new City(1, "Moscow"),
+                        true, LocalDateTime.now())
+        );
+        Model model = mock(Model.class);
+        PostService postService = mock(PostService.class);
+        when(postService.findAll()).thenReturn(posts);
+        HttpSession session = mock(HttpSession.class);
+        when(session.getAttribute("user")).thenReturn(null);
+        CityService cityService = mock(CityService.class);
+        PostController postController = new PostController(postService, cityService);
+        String page = postController.posts(model, session);
+        verify(model).addAttribute("posts", posts);
+        verify(model).addAttribute("user", new User());
         assertThat(page).isEqualTo("posts");
     }
 
