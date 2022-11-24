21377b649ab876e6a0b361b71ef6e25ef3c82b0b
Evgeny Novoselov <zkodru@gmail.com>
2022-11-24 12:42:57 +0300

    Поправил и добавил тестовые методы

--- src/test/java/ru/job4j/dreamjob/controller/PostControllerTest.java
+++ src/test/java/ru/job4j/dreamjob/controller/PostControllerTest.java
@@ -94,13 +94,16 @@ public class PostControllerTest {
 
     @Test
     public void whenCreatePost() {
-        Post input = new Post(1, "New post", "Description", new City(1, "Moscow"),
-                true, LocalDateTime.now());
+        City city = new City(1, "Москва");
+        Post post = mock(Post.class);
+        when(post.getCity()).thenReturn(city);
         PostService postService = mock(PostService.class);
         CityService cityService = mock(CityService.class);
+        when(cityService.findById(anyInt())).thenReturn(city);
         PostController postController = new PostController(postService, cityService);
-        String page = postController.createPost(input);
-        verify(postService).add(input);
+        String page = postController.createPost(post);
+        verify(post).setCity(city);
+        verify(postService).add(post);
         assertThat(page).isEqualTo("redirect:/posts");
     }
 
@@ -123,5 +126,40 @@ public class PostControllerTest {
         verify(model).addAttribute("cities", cities);
         verify(model).addAttribute("user", user);
         assertThat(page).isEqualTo("updatePost");
+    }
+
+    @Test
+    public void whenFormUpdatePostWithoutUser() {
+        Post post = new Post(1, "New Post", "Description", new City(1, "Москва"),
+                true, LocalDateTime.now());
+        List<City> cities = List.of(new City(1, "Москва"), new City(2, "СПб"));
+        Model model = mock(Model.class);
+        HttpSession session = mock(HttpSession.class);
+        when(session.getAttribute("user")).thenReturn(null);
+        PostService postService = mock(PostService.class);
+        when(postService.findById(anyInt())).thenReturn(post);
+        CityService cityService = mock(CityService.class);
+        when(cityService.getAllCities()).thenReturn(cities);
+        PostController postController = new PostController(postService, cityService);
+        String page = postController.formUpdatePost(model, 1, session);
+        verify(model).addAttribute("post", post);
+        verify(model).addAttribute("cities", cities);
+        verify(model).addAttribute("user", new User());
+        assertThat(page).isEqualTo("updatePost");
+    }
+
+    @Test
+    public void whenUpdatePost() {
+        City city = new City(1, "Москва");
+        Post post = mock(Post.class);
+        when(post.getCity()).thenReturn(city);
+        PostService postService = mock(PostService.class);
+        CityService cityService = mock(CityService.class);
+        when(cityService.findById(anyInt())).thenReturn(city);
+        PostController postController = new PostController(postService, cityService);
+        String page = postController.updatePost(post);
+        verify(post).setCity(city);
+        verify(postService).update(post);
+        assertThat(page).isEqualTo("redirect:/posts");
     }
 }
